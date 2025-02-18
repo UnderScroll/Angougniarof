2efb53e54dba513f4944785941d06de8faea1916
FaerDerr <matheo.souville@gmail.com>
2025-02-18 10:44:50 +0100

    laggy but working solution

--- Godot Project/Scenes/Game.gd
+++ Godot Project/Scenes/Game.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+const Pixelmatch := preload("res://addons/pixelmatch/pixelmatch.gd")
 var img1 : Image = null
 var img2 : Image = null
 
@@ -15,10 +15,17 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	img1 = $TextureRect.texture
+	img1 = $TextureRect.texture.get_image()
 
 func _on_button_2_pressed() -> void:
-	img2 = $TextureRect.texture
+	img2 = $TextureRect.texture.get_image()
 
 func _on_do_pressed() -> void:
+	var matcher := Pixelmatch.new()
+	matcher.threshold = 0.1
+	var img : Image = Image.create($TextureRect.texture.get_width(),$TextureRect.texture.get_height(),false,Image.FORMAT_RGBA8)
+	
+	var diff = matcher.diff(img1, img2, img, $TextureRect.texture.get_width(), $TextureRect.texture.get_height())
+	print(diff)
+	$TextureRect2.texture = ImageTexture.create_from_image(img)
 	pass # Replace with function body.
