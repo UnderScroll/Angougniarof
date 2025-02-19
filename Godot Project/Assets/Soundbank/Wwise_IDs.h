44c7c2fabb65f4216de40fe2d723d8d78f846af4
TITTARELLI SANZ-CRESPO Enzo <enzo.tittarelli-sanz-crespo.auditeur@lecnam.net>
2025-02-19 15:58:38 +0100

    feat(api)! : integration of Wwise 2024.1.1
    Introduced Wwise Authoring in Godot project (Angougniarof\Godot Project\Wwise)
    Wwise plugin integration itself is in Godot Project\addons\Wwise
    Indicating path of sound banks (Angougniarof\Godot Project\Assets\Soundbank)
    
    WARNING: could have to restart Godot project once to load Wwise Authoring API correctly

---
+++ Godot Project/Assets/Soundbank/Wwise_IDs.h
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_SCORE = 3240049516U;
+        static const AkUniqueID PLAY_STOP_TESTBLIP_LP = 2215418163U;
+        static const AkUniqueID PLAY_TESTBLIP = 1957084899U;
+        static const AkUniqueID PLAY_TESTBLIP3D = 2418185146U;
+        static const AkUniqueID PLAY_TESTBLIP3D_LP = 166791821U;
+        static const AkUniqueID PLAY_TESTBLIP_LP = 2541121866U;
+        static const AkUniqueID PLAY_TESTRANDOMBLIP = 768365626U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace HIGHSCORE
+        {
+            static const AkUniqueID GROUP = 2808377621U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID NEWRECORD = 820133554U;
+                static const AkUniqueID OLD = 1031377680U;
+            } // namespace SWITCH
+        } // namespace HIGHSCORE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TEST_RTPC_VOLUMELOOPTEST = 192803316U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSB = 1328360421U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
