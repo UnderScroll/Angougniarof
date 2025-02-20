929695f834c2a570f7449649cdf9ae44de3831ab
UnderScroll <tifabval@gmail.com>
2025-02-20 14:51:07 +0100

    Add : Compute score of difference

--- Godot Project/Scenes/Game.gd
+++ Godot Project/Scenes/Game.gd
@@ -4,27 +4,37 @@ const Pixelmatch := preload("res://addons/pixelmatch/pixelmatch.gd")
 var img1 : Image = null
 var img2 : Image = null
 
+@export var diff_texture: TextureRect;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var material: ShaderMaterial = $TextureRect.material;
-	material.set_shader_parameter("screen", $TextureRect.texture);
+	pass
 
-func _on_button_pressed() -> void:
-	img1 = $TextureRect.texture.get_image()
-
-func _on_button_2_pressed() -> void:
-	img2 = $TextureRect.texture.get_image()
-
-func _on_do_pressed() -> void:
-	var matcher := Pixelmatch.new()
-	matcher.threshold = 0.1
-	var img : Image = Image.create($TextureRect.texture.get_width(),$TextureRect.texture.get_height(),false,Image.FORMAT_RGBA8)
+func _on_get_score_button_pressed() -> void:
+	var start_time = Time.get_ticks_msec();
 	
-	var diff = matcher.diff(img1, img2, img, $TextureRect.texture.get_width(), $TextureRect.texture.get_height())
-	print(diff)
-	$TextureRect2.texture = ImageTexture.create_from_image(img)
-	printt($TextureRect.texture.get_width(), $TextureRect.texture.get_height())
+	var image: Image = diff_texture.get_texture().get_image();
+	
+	var sum: float = 0;
+	
+	var image_data = image.get_data();
+	
+	const pixel_ratio: float = 1.0 / 255.0;
+	
+	for i in range(0, image_data.size(), 4):
+		sum += image_data[i] * pixel_ratio;
+		
+	var score: float = 1 - sum / (image.get_width() * image.get_height());
+		
+	var end_time = Time.get_ticks_msec();
+	var elapsed_time = end_time - start_time;
+	
+	print("Got score of %s in %sms" % [score, elapsed_time]);
+
+func _on_set_ref_button_pressed() -> void:
+	var texture_rect = $SubViewport/ShaderPass/TextureRect;
+	texture_rect.material.set_shader_parameter("reference_image", texture_rect.texture);
